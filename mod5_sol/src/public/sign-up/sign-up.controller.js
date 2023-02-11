9ca2c9f79eb3eafa59c8bedfa18a9b05aa35a6dc
thebouda <a.baldo.upc@gmail.com>
2023-02-11 15:01:26 +0100

    added form mockup

---
+++ mod5_sol/src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,14 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+// SignUpController.$inject = ['menuItems'];
+function SignUpController() {
+  var $ctrl = this;
+  console.log("sign u0p controller");
+  // $ctrl.menuItems = menuItems;
+}
+
+})();
